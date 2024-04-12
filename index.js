bdbfee24f7ef7eccc21a9727773bc789bd8c607f
Vivek D <91778440+v-i-v-e-k-01@users.noreply.github.com>
2024-04-13 00:21:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require("body-parser");
 const res = require('express/lib/response');
+const { MongoClient } = require("mongodb");
+const dns = require("dns");
+const urlparser = require("url");
+
+
+const client= new MongoClient( process.env.DB_URL);
+const db = client.db("urlShortner");
+const newUrl = db.collection("newUrl");
+
+app.use(bodyParser.urlencoded({extended:false}));
+
 
 
 // Basic Configuration
@@ -26,25 +37,53 @@ app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
 
-app.use(bodyParser.urlencoded({extended:false}));
 
-var count=0;
+// var count=0;
 
-app.post("/api/shorturl", function(req,res){
+// // var newUrl=[];
 
-  const newUrl={
-    "original_url": req.body.url,
-    "shorturl": ++count
-  }
+app.post("/api/shorturl", function(req,res,next){
+  // const url = String(req.body.url);
+  // const hostname = new URL(url).hostname;
+  const dnslookup = dns.lookup(urlparser.parse(req.body.url).hostname, async function(err, address){
+    if (!address)
+    {
+      res.json({
+        "error": "invalid url"
+      })
+    }
+    else
+    {
+      const urlCount= await newUrl.countDocuments({});
+      // if( await newUrl.findOne({original_url:req.body.rl}))
+      // {
+      //   const urlDoc= await newUrl.findOne({original_url:req.body.rl});
+      // }
+      // else{
+        const urlDoc={
+          original_url: req.body.url,
+          short_url: urlCount
+        };
+      // }
+      const result = await newUrl.insertOne(urlDoc);
+      console.log(result);
+      res.json(urlDoc);
+    } 
+  })
+  // newUrl.push({
+  //   "original_url": req.body.url,
+  //   "shorturl": ++count
+  // });
 
-  res.json(newUrl);
-
-  generateShortUrl(newUrl, count);
+  // res.json(newUrl.find(url=> url.shorturl === count));
+  // next();
+  // generateShortUrl(newUrl, count);
 });
 
-const generateShortUrl= (newUrl, count)=>{
-  app.get("/api/shorturl/"+ count, function(req,res){
-    res.redirect(newUrl["original_url"]);
-  });
-};
+// const generateShortUrl= (newUrl, count)=>{
+app.get("/api/shorturl/:shorturl", async function(req,res){
+  const urlDoc= await newUrl.findOne({short_url:+req.params.shorturl});
+  res.redirect(urlDoc.original_url);   
+});
+// };
 
