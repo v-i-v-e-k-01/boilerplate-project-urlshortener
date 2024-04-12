0f07ee923490b726752b60d172f1abc0dd861924
Vivek D <vivek01110110@gmail.com>
2024-04-12 13:45:27 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require("body-parser");
+const res = require('express/lib/response');
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,3 +25,26 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+var count=0;
+
+app.post("/api/shorturl", function(req,res){
+
+  const newUrl={
+    "original_url": req.body.url,
+    "shorturl": ++count
+  }
+
+  res.json(newUrl);
+
+  generateShortUrl(newUrl, count);
+});
+
+const generateShortUrl= (newUrl, count)=>{
+  app.get("/api/shorturl/"+ count, function(req,res){
+    res.redirect(newUrl["original_url"]);
+  });
+};
+
